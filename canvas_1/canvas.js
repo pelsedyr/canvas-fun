a5af589057cdddeea301c16d5fb92d7609f336bd
Lukas Larsed <git@lukaslarsed.com>
2018-04-13 12:54:32 +0200

    circle colors

--- canvas_1/canvas.js
+++ canvas_1/canvas.js
@@ -5,14 +5,21 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+function randomHex(){
+    return `#${(Math.random()*0xFFFFFF<<0).toString(16)}`;
+}
+
 function Circle(x, y, r, dx, dy){
     this.x = x;
     this.y = y;
     this.radius = r;
     this.dx = dx;
     this.dy = dy;
+    var hex = randomHex();    
     
     this.draw = function() {
+        c.strokeStyle = hex;
+        c.fillStyle = hex;
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.closePath();
@@ -21,11 +28,13 @@ function Circle(x, y, r, dx, dy){
     }
 
     this.update = function(){
-        if(this.x + this.radius > innerWidth || 
+
+        if (this.x + this.radius > innerWidth || 
             this.x - this.radius < 0){
             this.dx = -this.dx;
         }
-        if(this.y + this.radius > innerHeight || 
+        
+        if (this.y + this.radius > innerHeight || 
             this.y - this.radius < 0){
             this.dy = -this.dy;
         }
@@ -42,7 +51,7 @@ for (var i = 0; i < 100; i++){
 
     var x = Math.random() * innerWidth;
     var y = Math.random() * innerHeight;
-    var radius = Math.floor((Math.random() * 20) + 1);
+    var radius = Math.floor((Math.random() * 30) + 1);
     var dx = (Math.random() - 0.5) * 8;
     var dy = (Math.random() - 0.5) * 8;
 
