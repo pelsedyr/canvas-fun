4d2e39e818716bdcb8108e9461c5582f162b485e
Lukas Larsed <git@lukaslarsed.com>
2018-04-13 10:18:53 +0200

    refactored circle

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -34,29 +34,41 @@ function getRandCoord(dir){
 console.log(getRandCoord('x'));
 console.log(getRandCoord('y'));
 
-var x = 100;
-var y = 100;
-var dx = 10;
-var dy = dx;
+function Circle(x, y, r, dx, dy){
+    this.x = x;
+    this.y = y;
+    this.radius = r;
+
+    this.draw = function() {
+        console.log('New circle', this.x, this.y);
+        c.beginPath();
+        c.arc(x, y, radius, 0, Math.PI * 2, false);
+        c.closePath();
+        c.stroke();
+    }
+
+    this.update = function(){
+        if(x + radius > innerWidth || x - radius < 0){
+            dx = -dx;
+        }
+        if(y + radius > innerHeight || y - radius < 0){
+            dy = -dy;
+        }
+    
+        x += dx;
+        y += dy;
+    }
+}
+
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 8;
+var dy = (Math.random() - 0.5) * 8;
 var radius = 20;
 
 function animate() {
-    // requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.closePath();
-    c.stroke();
-
-    if(x + radius > innerWidth || x - radius < 0){
-        dx = -dx;
-    }
-    if(y + radius > innerHeight || y - radius < 0){
-        dy = -dy;
-    }
-
-    x += dx;
-    y += dy;
+    requestAnimationFrame(animate);
+    var circle = new Circle(x, y, radius, dx, dy);
 }
 
 animate();
